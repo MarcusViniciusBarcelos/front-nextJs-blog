b685eecb1b05922e13e927776439516a3ef80515
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-02-16 22:41:59 -0300

    atualizando logo

--- src/components/LogoLink/stories.tsx
+++ src/components/LogoLink/stories.tsx
@@ -6,7 +6,8 @@ export default {
   component: LogoLink,
   args: {
     text: 'LogoLink',
-    srcImg: 'https://ionicframework.com/docs/img/frameworks/react.svg',
+    srcImg:
+      'https://res.cloudinary.com/dm5kvicsz/image/upload/v1708022537/Whats_App_Image_2023_07_13_at_15_32_56_000d5b42b2.jpg',
     link: 'http://localhost',
     newTab: false,
   },
