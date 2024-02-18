e006662823e41364fb083eba53754d69d8fbba84
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-02-18 15:18:20 -0300

    Componente Header

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,9 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Marcus Barcelos',
+  blogDescription:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+  logo: 'https://res.cloudinary.com/dm5kvicsz/image/upload/v1708022537/Whats_App_Image_2023_07_13_at_15_32_56_000d5b42b2.jpg',
+  showText: true,
+} as HeaderProps;
