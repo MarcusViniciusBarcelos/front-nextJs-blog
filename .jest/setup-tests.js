9b1ac7dd2c9f02542ee619ea259c23f88414a806
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-02-20 14:53:22 -0300

    testes Componente PostCard - 100% coverage

--- .jest/setup-tests.js
+++ .jest/setup-tests.js
@@ -1,2 +1,18 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+const originalError = console.error;
+
+console.error = function(...args) {
+  if (
+    args.length > 0 &&
+    typeof args[0] === 'string' &&
+    (
+      args[0].includes('Warning: Received `false` for a non-boolean attribute `uppercase`. ') ||
+      args[0].includes('Warning: React does not recognize the `colorDark` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `colordark` instead. If you accidentally passed it from a parent component, remove it from the DOM element. ')
+    )
+  ) {
+    return;
+  }
+  originalError;
+};
--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -15,7 +15,7 @@ export const PostCard = ({ slug, title, cover, excerpt }: PostCardProps) => {
   return (
     <Styled.Wrapper>
       <Link href={`/post/${slug}`} passHref>
-        <Styled.Cover src={cover.url} />
+        <Styled.Cover src={cover.url} alt={title} />
       </Link>
       <Link style={{ textDecoration: 'none' }} href={`/post/${slug}`} passHref>
         <Heading as="h2" size="small">
