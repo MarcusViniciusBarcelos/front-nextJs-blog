b594143496f9098074eabf168afd8268053741f5
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-02-29 17:22:52 -0300

    voltando para haroku (esqueci)

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ export default {
   url: 'https://strapy-blog-3898898c84dd.herokuapp.com',
   siteName: 'Marcus Barcelos',
   defaultSlug: 'landing-page',
-  // graphqlURL: 'https://strapy-blog-3898898c84dd.herokuapp.com/graphql/',
-  graphqlURL: 'http://127.0.0.1:1337/graphql/',
+  graphqlURL: 'https://strapy-blog-3898898c84dd.herokuapp.com/graphql/',
+  // graphqlURL: 'http://127.0.0.1:1337/graphql/',
 };
